16903af1d6ec6fac80ca461dbc15e065aa1d1afc
lanjingling0510 <463103063@qq.com>
2015-10-12 14:52:24 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -2,48 +2,60 @@
 var path = require('path');
 var webpack = require('webpack');
 var cssnext = require('cssnext');
-
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var postcssImport = require('postcss-import');
+var postMixins = require('postcss-mixins');
+var postNested = require('postcss-nested');
 
 /* eslint-enable */
 module.exports = {
     context: path.join(__dirname, '/src'),
     entry: {
-        vendor: ["jquery"],
+        vendor: ['jquery'],
         home: './home',
-        login: './login'
+        login: './login',
     },
     output: {
-        path: path.join(__dirname, '/dist/js'),
-        filename: '[name].bundle.js',
+        path: path.join(__dirname, '/dist'),
+        filename: 'js/[name].bundle.js',
     },
     module: {
         loaders: [
-            { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
-            { test: /\.css$/, loader: 'style!css!postcss-loader'},
-            { test: /\.json$/, loader: 'json' },
-            { test: /\.(png|jpg)$/, loader: 'url?limit=25000' },
-            { test: /\.html$/, exclude: /node_modules/, loader: 'html!html-minify' },
-            { test: /\.(ttf|eot|svg|otf)(\?v=\d(\.\d){2})?$/, loader: 'file' },
-            { test: /\.woff(2)?(\?v=\d(\.\d){2})?$/, loader: 'url?limit=10000&minetype=application/font-woff'},
+            {test: /\.js$/, exclude: /node_modules/, loader: 'babel'},
+            {test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!postcss-loader')},
+            {test: /\.json$/, loader: 'json'},
+            {test: /\.(png|jpg)$/, loader: 'url?limit=25000'},
+            {test: /\.html$/, exclude: /node_modules/, loader: 'html!html-minify'},
+            {test: /\.(ttf|eot|svg|otf)(\?v=\d(\.\d){2})?$/, loader: 'file'},
+            {test: /\.woff(2)?(\?v=\d(\.\d){2})?$/, loader: 'url?limit=10000&minetype=application/font-woff'},
         ],
     },
-
-    postcss: [cssnext({
-        features: {
-            customProperties: {
-                variables: {
-                    mainColor: "red",
-                    altColor: "blue",
-                }
-            }
-        }
-    })],
+    postcss: function () {
+        return [
+            postcssImport({
+                onImport: function (files) {
+                    files.forEach(this.addDependency);
+                }.bind(this),
+            }),
+            cssnext({
+                features: {
+                    'browers': ['last 2 version'],
+                },
+            }),
+            postMixins,
+            postNested,
+        ];
+    },
     plugins: [
+        new ExtractTextPlugin('css/[name].css?[hash]-[chunkhash]-[contenthash]-[name]', {
+            disable: false,
+            allChunks: true,
+        }),
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery',
             _: 'lodash',
         }),
-        new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
+        new webpack.optimize.CommonsChunkPlugin('vendor', 'js/vendor.bundle.js'),
     ],
 };
